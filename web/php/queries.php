7a61d40a89098266331fb1c63fccbabfe34e992a
Manuel Lopez <manny.lopez625@gmail.com>
2014-12-01 22:46:47 -0500

    fixing multiple queries

--- web/php/queries.php
+++ web/php/queries.php
@@ -7,31 +7,35 @@ include 'DBConnect.php';
 
 $conn = openConnect();
 
+print_r($_POST);
+
 if (isset($_POST['queries'])){
-	$search=$_POST['queries'];
+    echo "[]";
+	$search=array($_POST['queries']);
 }else{
+    echo "not set";
 	$search = array();
 }
-
+echo "</br>";
 if($search){
     foreach($search as $s){
-        echo $s;
         $tkn = explode(" : ", $s);
         echo $tkn[0];
+        echo "</br>";
         if($tkn[0]=='web rating'){
             $dbQuery = 'SELECT * FROM TestGames WHERE web_rating='.$tkn[1];
-            echo $dbQuery;
             $result = $conn->query($dbQuery);
             while($row = $result->fetch_assoc()){
                 echo $row['title'];
+                echo "</br>";
             }
         }
         else if ($tkn[0]=='user rating') {
             $dbQuery = 'SELECT * FROM TestGames WHERE user_rating='.$tkn[1];
-            echo $dbQuery;
             $result = $conn->query($dbQuery);
             while($row = $result->fetch_assoc()){
                 echo $row['title'];
+                echo "</br>";
             }
 
         }
