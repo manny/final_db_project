1864c91ee31c4387d80438a081abb5e63eeaf9c6
Matt Klein <shesso8bit@gmail.com>
2014-12-02 15:51:59 -0500

    working array, finally

--- web/php/queries.php
+++ web/php/queries.php
@@ -1,5 +1,3 @@
-<html>
-<body>
 
 	<?php
 
@@ -7,38 +5,35 @@
 
 	$conn = openConnect();
 
-	print_r($_POST);
+	//print_r($_POST);
 
-	/*
 	if (isset($_POST['queries'])){
-		echo "[]";
-		$search=array($_POST['queries']);
+		$search=$_POST['queries'];
 	}else{
-		echo "not set";
 		$search = array();
 	}
-	*/
 
-	$search = array($_POST['queries']);
+	/*
+	$search = $_POST['queries'];
 
 	foreach($search as $s){
 		echo "$s";
 	};
+	*/
 
-	echo "</br>";
-	//print_r($search);
-	echo "</br>";
+	//	print_r($search);
+	$out = array(array());
+	$testForNow = array();
 	if($search){
 		foreach($search as $s){
 			$tkn = explode(" : ", $s);
-			echo $tkn[0];
-			echo "</br>";
 			if($tkn[0]=='web rating'){
 				$dbQuery = 'SELECT * FROM TestGames WHERE web_rating='.$tkn[1];
 				$result = $conn->query($dbQuery);
 				while($row = $result->fetch_assoc()){
-					echo $row['title'];
-					echo "</br>";
+					//echo $row['title'];
+					$out[]=array("title"=>$row['title'],"web_rating"=>$row['web_rating'],"user_rating"=>$row['user_rating'],"Developer"=>$row['Developer'],"Publisher"=>$row['Publisher'],"Genres"=>$row['Genres']);
+
 				}
 			}
 			else if ($tkn[0]=='user rating') {
@@ -46,7 +41,6 @@
 				$result = $conn->query($dbQuery);
 				while($row = $result->fetch_assoc()){
 					echo $row['title'];
-					echo "</br>";
 				}
 
 			}
@@ -55,6 +49,40 @@
 
 	?>
 
-
+	<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../lib/datatables/media/css/jquery.dataTables.css" />
+		<link rel="stylesheet" type="text/css" href="../stylesheets/base.css" />
+		<link rel="stylesheet" type="text/css" href="../stylesheets/skeleton.css" />
+		<link rel="stylesheet" type="text/css" href="../stylesheets/layout.css" />
+		<script type="text/javascript" src="../js/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="../lib/datatables/media/js/jquery.dataTables.js"></script>
+	</head>
+	<script>
+	$(document).ready(function(){
+		$('#myTable').DataTable();
+	});
+	</script>
+	<body>
+		<div class="container" style="top:50px">
+			<h1 class="remove-bottom"><a href="../index.php">Game Project!</a></h1>
+			<br>
+			<hr>
+			<br>
+			<table cellpadding="0" cellspacing="0" border="0" class="display" id="myTable" width="100%">
+				<thead>
+					<tr>
+						<th> Title </th>
+						<th> Web Rating </th>
+						<th> User Rating </th>
+						<th> Developer </th>
+						<th> Publisher </th>
+						<th> Genres </th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		</div>
 </body>
-</html>
+</head>
