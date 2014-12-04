795529fbc45fcdb2285672bfdc76e1a021b83f80
root <david.fu92@gmail.com>
2014-12-03 23:20:03 -0500

    fix query

--- web/php/rec.php
+++ web/php/rec.php
@@ -14,4 +14,10 @@ select * from Owns,Games,Tags,Players where Players.GivenName=Tags.GivenName and
 
 Select distinct title from TestGames g,Players,Owns where Players.GivenName='Samuel' and Owns.First_Name=Players.GivenName and g.Genres Like Concat("%",Players.Preferred_Genre,"%") and Owns.Game<>g.title and g.web_rating+g.user_rating=(Select max(web_rating+user_rating) from TestGames g,Players,Owns where Players.GivenName='Samuel' and Owns.First_Name=Players.GivenName and g.Genres Like Concat("%",Players.Preferred_Genre,"%") and Owns.Game<>g.title);
 
+
+
+
+
+Select distinct title from TestGames g,Players,Owns where Players.GivenName=Tags.GivenName and Players.Surname=Tags.Surname and Tags.Tag='INPUT' and Owns.First_Name=Players.GivenName and g.Genres Like Concat("%",Players.Preferred_Genre,"%") and Owns.Game<>g.title and g.web_rating+g.user_rating=(Select max(web_rating+user_rating) from TestGames g,Players,Owns where Players.GivenName=Tags.GivenName and Players.Surname=Tags.Surname and Tags.Tag='INPUT' and Owns.First_Name=Players.GivenName and g.Genres Like Concat("%",Players.Preferred_Genre,"%") and Owns.Game<>g.title);
+
 ?>
